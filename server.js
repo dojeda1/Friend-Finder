119e94dc2b0c621487454431aa9a1d8eea428ea8
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-05-03 18:09:26 -0600

    When Survey form is submitted a new object is addded to the profiles array on the server.

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+// Profiles
+var profiles = [{
+    name: "Bobert",
+    photo: "https://i.pinimg.com/originals/15/64/01/1564012a2a780ee83de9cacbf2e0e7ba.jpg",
+
+    q1: "1",
+    q2: "1",
+    q3: "1",
+    q4: "1",
+    q5: "1",
+    q6: "1",
+    q7: "1",
+    q8: "1",
+    q9: "1",
+    q10: "1"
+}];
+
 // Starter
 
 app.get("/", function (req, res) {
@@ -20,8 +37,24 @@ app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "./app/public/survey.html"));
 });
 
+app.get("/api/profiles", function (req, res) {
+    return res.json(profiles);
+});
 
+app.post("/survey", function (req, res) {
 
+    var newProfile = req.body;
+
+    newProfile.routeName = newProfile.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newProfile);
+
+    profiles.push(newProfile);
+
+    res.json(newProfile);
+});
+
+// Server start listening
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
