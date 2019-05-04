5f48c213aa8cda0357440c3a4faaca55727f8aad
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-05-04 13:39:38 -0600

    Submit button grabs infor from server.

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+// Profiles
+var profiles = [{
+        routeName: "bobert",
+        name: "Bobert",
+        photo: "https://i.pinimg.com/originals/15/64/01/1564012a2a780ee83de9cacbf2e0e7ba.jpg",
+
+        q1: "1",
+        q2: "1",
+        q3: "1",
+        q4: "1",
+        q5: "1",
+        q6: "1",
+        q7: "1",
+        q8: "1",
+        q9: "1",
+        q10: "1"
+    },
+    {
+        routeName: "clarice",
+        name: "Clarice",
+        photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/04/13/11/woman-on-bus.jpg?w968h681",
+
+        q1: "5",
+        q2: "5",
+        q3: "5",
+        q4: "5",
+        q5: "5",
+        q6: "5",
+        q7: "5",
+        q8: "5",
+        q9: "5",
+        q10: "5"
+    }
+];
+
+module.exports = profiles;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require("express");
 var path = require("path");
 
+var profiles = require("./app/data/friends.js")
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -9,23 +11,6 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
-
-// Profiles
-var profiles = [{
-    name: "Bobert",
-    photo: "https://i.pinimg.com/originals/15/64/01/1564012a2a780ee83de9cacbf2e0e7ba.jpg",
-
-    q1: "1",
-    q2: "1",
-    q3: "1",
-    q4: "1",
-    q5: "1",
-    q6: "1",
-    q7: "1",
-    q8: "1",
-    q9: "1",
-    q10: "1"
-}];
 
 // Starter
 
@@ -37,11 +22,25 @@ app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "./app/public/survey.html"));
 });
 
-app.get("/api/profiles", function (req, res) {
+app.get("/api/friends", function (req, res) {
     return res.json(profiles);
 });
 
-app.post("/survey", function (req, res) {
+app.get("/api/friends/:profile", function (req, res) {
+    var chosen = req.params.profile;
+
+    console.log(chosen);
+
+    for (var i = 0; i < profiles.length; i++) {
+        if (chosen === profiles[i].routeName) {
+            return res.json(profiles[i]);
+        }
+    }
+
+    return res.json(false);
+});
+
+app.post("/api/friends", function (req, res) {
 
     var newProfile = req.body;
 
